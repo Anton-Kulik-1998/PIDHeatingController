eb2629637b08ce1608caae12fd08b6f52086da55
Антон Кулик <anton.smile.1998@gmail.com>
2022-08-03 15:03:22 +0300

    Проверка LCD1602

--- PIDHeaterController/src/main.cpp
+++ PIDHeaterController/src/main.cpp
@@ -1,9 +1,37 @@
 #include <Arduino.h>
+#include <Wire.h>
+// базовый пример инициализации и вывода текста
+#include <LiquidCrystal_I2C.h>  // подключаем библу
+// адрес дисплея 0x3f или 0x27
+// размер дисплея 16x2 (поддерживаются и другие, например 20x4)
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // адрес, столбцов, строк
 
-void setup() {
-  // put your setup code here, to run once:
+String test = "";
+
+void lcdTest() {
+  if (Serial.available() > 0) {
+    lcd.clear();
+    lcd.home();
+    lcd.print(test);
+    lcd.setCursor(0, 1);
+    test = Serial.readString();
+    lcd.print(test);
+  }
 }
 
+void setupLcd() {
+  lcd.init();           // инициализация
+  lcd.backlight();      // включить подсветку
+  lcd.setCursor(1, 0);  // столбец 1 строка 0
+  lcd.print("Hello, world!");
+  lcd.setCursor(4, 1);  // столбец 4 строка 1
+  lcd.print("Kulik");
+}
+
+void setup() {
+  Serial.begin(9600);
+  setupLcd();
+}
 void loop() {
-  // put your main code here, to run repeatedly:
+  lcdTest();
 }
