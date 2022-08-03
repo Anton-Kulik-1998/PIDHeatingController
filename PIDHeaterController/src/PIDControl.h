58f6fd95b12075b8226ffee3cde967e154963eb2
Антон Кулик <anton.smile.1998@gmail.com>
2022-08-03 15:20:52 +0300

    Добавлена поддержка PID алгоритма

---
+++ PIDHeaterController/src/PIDControl.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <Arduino.h>
+
+#include "GyverPID.h"
+
+//Функция с ПИДом + Отрисовка графиков в Serial
+void pidCountrol() {
+  static uint32_t tmr;
+  if (millis() - tmr > 50) {
+    tmr = millis();
+
+    pid.Kp = settings.Kp;  //Даём эти переменные коэффициентам ПИД
+    pid.Ki = settings.Ki;
+    pid.Kd = settings.Kd;
+
+    pid.setpoint = settings.setpoint;  //Это требуемое значение, тут нужно
+                                       //выставить температуру
+
+    sensors.requestTemperatures();  //Сделать опрос датчика темп.
+    pid.input =
+        sensors.getTempCByIndex(0);  //Дать на вход значение с датчика темп.
+    pid.getResult();                 //выполнить ПИД вычисления
+    analogWrite(15, pid.output);    // пин, к которому подключен драйвер
+
+    //Графики в serial
+    Serial.print(pid.input);
+    Serial.print(' ');
+    Serial.print(pid.getResult());
+    Serial.print(' ');
+    Serial.println(pid.setpoint);
+  }
+}
+
+void setupPID() {
+  pid.setDt(50);             // Присваиваем ПИДу период DT
+  pid.setDirection(NORMAL);  //Направление регулирования, HORMAL - для обогрева, REVERSE - для охлаждения
+}
+
+GyverPID pid;
